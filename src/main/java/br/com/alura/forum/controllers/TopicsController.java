6ba5b170853fdc7a7c7861d9bae156784635817b
Paulo Patarello <paulohenriquepatarello@gmail.com>
2021-05-14 22:27:04 -0300

    Adicionando TopicRepository

--- src/main/java/br/com/alura/forum/controllers/TopicsController.java
+++ src/main/java/br/com/alura/forum/controllers/TopicsController.java
@@ -1,26 +1,27 @@
 package br.com.alura.forum.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controllers.dto.TopicDTO;
 import br.com.alura.forum.models.Topic;
+import br.com.alura.forum.repository.TopicRepository;
 
 @RestController
 @RequestMapping("/topics")
 public class TopicsController {
+	
+	@Autowired
+	private TopicRepository topicRepo;
 
 	@GetMapping
 	public List<TopicDTO> getTopics() {
-		Topic topic = new Topic();
-		topic.setTitle("Spring Rest");
-		topic.setMessage("How can I learn Spring Rest?");
-		
-		return TopicDTO.convertList(Arrays.asList(topic, topic));
+		List<Topic> topics = topicRepo.findAll();
+		return TopicDTO.convertList(topics);
 	}
 	
 }
